2f05422bb6f30b755ada43ab0b4675f969f2a2b8
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-12 15:11:10 +0500

    init

---
+++ IJ Buy crystals/IJ Buy crystals/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IJ_Buy_crystals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystals = 50;
+            int numberCrystals;
+            int costCrystals;
+            Console.WriteLine("В ведите количество золота");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Какое количестов крмсталлов вы хотите купить");
+            numberCrystals = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Цене кристалла");
+            costCrystals = Convert.ToInt32(Console.ReadLine());
+            crystals -= numberCrystals;
+            numberCrystals *= costCrystals;
+            gold -= numberCrystals;
+            if (costCrystals > gold)
+            {
+                Console.WriteLine("Недостаточно золота для покупки кристаллов");
+            }
+            else
+            {
+                Console.WriteLine($"остаток золота {gold} остаток кристаллов {crystals}");
+            }
+        }
+    }
+}
